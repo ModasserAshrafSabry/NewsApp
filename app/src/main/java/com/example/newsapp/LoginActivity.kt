888a2f312b12031c4fa27e9e2e68fe306e0f8561
abdelrahman-zaher <abdelrahman.zaher55@gmail.com>
2025-10-13 14:04:03 +0300

    zaher sec. update (firebase and logic)

--- app/src/main/java/com/example/newsapp/LoginActivity.kt
+++ app/src/main/java/com/example/newsapp/LoginActivity.kt
@@ -1,24 +1,73 @@
+
 package com.example.newsapp
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.newsapp.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+import com.google.firebase.auth.FirebaseAuth
+
+lateinit var auth: FirebaseAuth
+
+class LoginActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//instance firebase
+        auth = FirebaseAuth.getInstance()
 
+
+        binding.singnupTxt.setOnClickListener {
+            startActivity(Intent(this, signUpActivity::class.java))
+        }
+
+
+        binding.logBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passEt.text.toString()
+
+            if(email.isEmpty() || pass.isEmpty()) {
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            }
+            //هنا بنبعت ل فاير بيز الاميل والباسورد وبنشوف هل العمليه نجحت ولا لا
+            auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser  //بنخزن بيانات المستخدم
+                    if (user != null && user.isEmailVerified) {
+                        startActivity(Intent(this, CategoryActivity::class.java))
+                        finish()  //لو اليوزر عامل تاكيد للايميل والبيانات بتاعته اتبعتت فعلا هيخش علي ال category
+                    } else { //لو مش عامل تاكيد هبعتله ياكد
+                        Toast.makeText(this, "Please verify your email first", Toast.LENGTH_LONG).show()
+                        auth.signOut()
+                    }
+                } else { // لو حصل اي مشكله بنبعتله رساله بالمشكله اللي حصلت
+                    Toast.makeText(this, "Login failed: ${task.exception?.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
+
+
+
+
+
+
 }
+
+
+
+
--- app/src/main/java/com/example/newsapp/signupActivity.kt
+++ app/src/main/java/com/example/newsapp/signupActivity.kt
@@ -1,20 +1,71 @@
 package com.example.newsapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.newsapp.CategoryActivity
+import com.example.newsapp.R
+import com.example.newsapp.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class signupActivity : AppCompatActivity() {
+
+class signUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivitySignupBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //بخزن الايميل والباسورد
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passEt.text.toString()
+            //بتاكد ان اليوزر مش سايب اي خانه فاضيه
+            if (binding.nameEt.text.isEmpty()) binding.nameLy.error = "Enter your name"
+            else if (email.isEmpty()) binding.emailLy.error = "Enter your email"
+            else if (pass.isEmpty()) binding.passLy.error = "Enter your password"
+            // لو confirm pass مش زي ال pass
+            else if (binding.confirmPassEt.text.toString() != pass) binding.passLy.error =
+                "Passwords should match"
+            else {
+                val auth = FirebaseAuth.getInstance()
+                //ببعت البيانات ل firebase عشان يخزنها واتاكد ان الايميل مش مستخدم قبل كده
+                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser //بخزن البيانات
+
+                        //هبعت ايميل ال verfiey لو اليوزر بيناته مبعوته ومش مبعوته null وهيطلعله رساله انه اتبعت ويرجعه ل صفحه ال login
+                        user?.sendEmailVerification()?.addOnCompleteListener { verifyTask ->
+                            if (verifyTask.isSuccessful) {
+                                Toast.makeText(
+                                    this, "Verification email sent to $email", Toast.LENGTH_LONG
+                                ).show()
+                                startActivity(Intent(this, LoginActivity::class.java))
+                                finish()
+                            }
+                        }
+
+                    } else { //لو حصل اي مشكله هيطلعلو رساله بيها
+                        Toast.makeText(
+                            this, "Signup failed: ${task.exception?.message}", Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+            }
+        }
+
+
     }
+
+
 }
