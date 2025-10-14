edf98b49bf01e484e27ac618ef8463ed295873f6
Mahmoud-K-Nassar <thkilerboyyyy@gmail.com>
2025-10-14 21:18:07 +0300

    change icon

--- app/src/main/java/com/example/newsapp/SplashActivity.kt
+++ app/src/main/java/com/example/newsapp/SplashActivity.kt
@@ -41,7 +41,7 @@ class SplashActivity : AppCompatActivity() {
         scaleY.duration = 1500
 
         val moveUp = ObjectAnimator.ofFloat(logo, "translationY", 0f, -700f)
-        moveUp.duration = 1200
+        moveUp.duration = 900
         moveUp.interpolator = DecelerateInterpolator()
 
         val logoAnim = AnimatorSet()
