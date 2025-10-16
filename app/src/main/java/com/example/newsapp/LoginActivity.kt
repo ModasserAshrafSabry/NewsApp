5cce628face8e4806a9ddf2f8db682fa40bd6c8c
Mahmoud-K-Nassar <thkilerboyyyy@gmail.com>
2025-10-17 02:14:58 +0300

    font done

--- app/src/main/java/com/example/newsapp/LoginActivity.kt
+++ app/src/main/java/com/example/newsapp/LoginActivity.kt
@@ -62,7 +62,7 @@ class LoginActivity : AppCompatActivity() {
                 } else { // لو حصل اي مشكله بنبعتله رساله بالمشكله اللي حصلت
                     Toast.makeText(
                         this,
-                        "Login failed: ${task.exception?.message}",
+                        "Login failed:Please check your email and password",
                         Toast.LENGTH_LONG
                     ).show()
                 }
